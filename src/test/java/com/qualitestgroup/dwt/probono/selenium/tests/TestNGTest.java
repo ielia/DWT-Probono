c965c60e9a3114222ff7b462868639ead15fe916
Ignacio Elia <Ignacio.Elia@qualitestgroup.com>
2022-09-26 12:55:49 -0300

    Added RemoteWebDriver use.

--- src/test/java/com/qualitestgroup/dwt/probono/selenium/tests/TestNGTest.java
+++ src/test/java/com/qualitestgroup/dwt/probono/selenium/tests/TestNGTest.java
@@ -3,7 +3,10 @@ package com.qualitestgroup.dwt.probono.selenium.tests;
 import com.qualitestgroup.dwt.probono.selenium.pageobjects.Page;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.testng.ITest;
 import org.testng.ITestContext;
@@ -24,7 +27,19 @@ public class TestNGTest implements ITest {
      * Name of the System Property specifying browser type.
      */
     public static final String BROWSER_NAME_PROPERTY = "browser";
+    /**
+     * Name of the System Property specifying Selenium hub URL.
+     */
+    public static final String SELENIUM_HUB_URL_PROPERTY = "hub.url";
 
+    /**
+     * Browser name.
+     */
+    protected String browserName = System.getProperty(BROWSER_NAME_PROPERTY);
+    /**
+     * Selenium Hub URL, can be null.
+     */
+    protected String seleniumHubURL = System.getProperty(SELENIUM_HUB_URL_PROPERTY);
     /**
      * Current page per test thread.
      */
@@ -53,14 +68,16 @@ public class TestNGTest implements ITest {
         SLF4JBridgeHandler.install();
         // TODO: Find a better way to do this
         System.setProperty("webdriver.chrome.silentOutput", "true");
-        System.setProperty("webdriver.edge.silentOutput", "true");
+        // System.setProperty("webdriver.edge.silentOutput", "true");
         System.setProperty("webdriver.firefox.logfile", "/dev/null");
         System.setProperty("webdriver.ie.driver.silent", "true");
         System.setProperty("webdriver.opera.silentOutput", "true");
 
         currentPages.set(new HashMap<>(1));
-        webDriverManager = WebDriverManager.getInstance(System.getProperty(BROWSER_NAME_PROPERTY));
-        webDriverManager.setup();
+        if (seleniumHubURL == null) {
+            webDriverManager = WebDriverManager.getInstance(browserName);
+            webDriverManager.setup();
+        }
     }
 
     /**
@@ -120,7 +137,16 @@ public class TestNGTest implements ITest {
     protected void beforeEach(Method method, ITestContext context, Object[] data) {
         String template = method.getAnnotation(Test.class).testName();
         this.testName.set(String.format(StringUtils.isBlank(template) ? method.getName() : template, data));
-        WebDriver configuredDriver = webDriverManager.create();
+
+        WebDriver configuredDriver;
+        if (seleniumHubURL == null) {
+            configuredDriver = webDriverManager.create();
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browserName);
+            capabilities.setPlatform(Platform.WINDOWS);
+            configuredDriver = new RemoteWebDriver(capabilities);
+        }
         this.driver.set(configuredDriver);
     }
 
